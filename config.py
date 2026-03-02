3e2fa47d020d3ebca6daaae404ea8d74db2d981d
ReallyRichKid <126290726+ReallyRichKid@users.noreply.github.com>
2026-03-02 03:43:18 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "8379765561:AAEmuUvsYx5UUdL1htPfXIe_7TRNPdld0Ac" # токен бота
+
+ADMIN_ID = 8595183705 # ид админа
+
+CRYPTOBOT_TOKEN = "541579:AAHxBDoBhX3q8nOXxFankGftJK8BEHow5m8" # токен crypto pay 
+
+CRYPTOBOT_API_URL = 'https://pay.crypt.bot/api/' # ссылка
+
+DB_CHANNEL_ID = "-1003798504280" # для хранения товаров, обязательно добавить бота в канал
+
+# ссылки 1 - testnet ; 2 - crypto bot
+# 1 - https://testnet-pay.crypt.bot/api/
+# 2 - https://pay.crypt.bot/api/
